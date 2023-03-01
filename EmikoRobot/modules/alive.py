bf17fc27d6ee75e9e53a5a7bf0927778e1aac53a
Radenihbos <113280187+Radenihbos@users.noreply.github.com>
2023-03-01 18:27:00 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO = "https://i.postimg.cc/ZYp6yCw3/IMG-20230301-134109-967.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), Yo mek Aing OtanxRobot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [RADEN](https//t.me/Radenihboss)** \n\n"
+  TEXT += f"⚪ **My Master : [NAS X ROBOT](https//t.me/Radenihboss)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Nasnihbot?start=help"), Button.url("Support", "https://t.me/Jombloday")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Nasnihbot?start=help"), Button.url("Support", "https://t.me/Nasnihbos")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
