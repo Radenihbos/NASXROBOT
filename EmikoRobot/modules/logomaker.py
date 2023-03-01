7ffd191a2043391a3d6c5011828b15981f9294e2
Radenihbos <113280187+Radenihbos@users.noreply.github.com>
2023-03-01 18:31:14 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [NAS X ROBOT](https://t.me/Nasnihbot)",
             force_document=False,
         )
         os.remove(flnme)
